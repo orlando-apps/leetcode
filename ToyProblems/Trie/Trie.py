51271a2b9a20ab3abb18dcff3570a045e9fe1ba6
Orlando <jcsf1991@gmail.com>
2023-02-26 04:40:24 -0800

    add Trie data structure comments

--- ToyProblems/Trie/Trie.py
+++ ToyProblems/Trie/Trie.py
@@ -55,42 +55,56 @@ def deleteString(root, word, index):
 
   # case 1
   if len(currentNode.children) > 1:
-      deleteString(currentNode, word, index + 1)
-      return False
+    print('case 1')
+    deleteString(currentNode, word, index + 1)
+    return False
 
   # case 2
   if index == len(word) - 1:
     # this word is not in trie anymore (ex. contains at least 1 character of 'S'))
     if len(currentNode.children) >= 1:
+      print('case 2 - keep')
       # last character and endOfString for last character use the same node
       currentNode.endOfString = False
       return False
     else:
+      print('case 2 - delete', ch)
       # since this is the last character, we should return true, that it is okay to delete
       root.children.pop(ch)
       return True
 
   # case 3
   if currentNode.endOfString == True:
+    print('case 3')
     # cannot 'delete' it b/c some other string uses this node, so move onto the next
     deleteString(currentNode, word, index + 1)
     return False
 
   # case 4
+  # where currentNode.children == 1, but is it shared with another word?
+  print('case 4')
   canThisNodeBeDeleted = deleteString(currentNode, word, index + 1)
   if canThisNodeBeDeleted == True:
+    #this is not shared, ex: goes from case 4 to case 2 - keep (delete: bk, keep: api)
+    # if this is one character, and the next we can delete, then we know we can also delete this one
+    print('case 4 - delete', ch)
     root.children.pop(ch)
     return True
   else:
+    #this is shared, ex: goes from case 4 to case 1 (delete: api, keep: apple)
+    #this is shared, ex: goes from case 4 to case 2 - keep (delete: api, keep: apis)
+    #this is shared, ex: goes from case 4 to case 3 (delete: apis, keep: api)
+    print('case 4 - keep')
     return False
 
 
 newTrie =Trie()
-newTrie.insertString('App')
-newTrie.insertString('Api')
-deleteString(newTrie.root, 'Api', 0)
-print(newTrie.searchString('Api'))
+newTrie.insertString('api')
+newTrie.insertString('bk')
+# newTrie.insertString('bk')
+print(deleteString(newTrie.root, 'bk', 0))
 
+# print(newTrie.searchString('Api'))
 
 
 
